9cbb2ff5b7328ec4ccc77bd34f834b75034454f8
Krypton Cougars <kryptoncougars@pasd.us>
2016-09-15 20:07:40 -0400

    set initial values for index wheel

--- subsystems/shooter.py
+++ subsystems/shooter.py
@@ -1,25 +1,34 @@
-from  wpilib import digitalInput.py     
+from  wpilib import digitalInput
 from wpilib.cantalon import CANTalon
 
-class Shooter():
+class Shooter(Subsystem):
+    def __init__(self, name):
+        super().__init__(name)
     ballDetector = digitalInput(0)
     leftPivorMotor = CANTalon(9)
     rightPivotMotor = CANTalon(8)
     indexWheel = CANTalon(10)
     shooterWheel = CANTalon(11)
+    settingsLoaded = False
     
-    rightPivotMotor.setControlMode(CANTalon.ControlMode.Position);
+    indexWheel.setControlMode(CANTalon.ControlMode.PercentVbus)
+    # Set brake mode to coast.
+    indexWheel.enableBrakeMode(False)
+    indexWheel.setInverted(True)
+    
+    rightPivotMotor.setControlMode(CANTalon.ControlMode.Position)
     
     # P = .003, I = 0, D = 0
-    rightPivotMotor.setPID(.003, 0, 0);
-    rightPivotMotor.configMaxOutputVoltage(4);
-    rightPivotMotor.SetInverted(true);
+    rightPivotMotor.setPID(.003, 0, 0)
+    rightPivotMotor.configMaxOutputVoltage(4)
+    rightPivotMotor.SetInverted(true)
     
     leftPivotMotor.setControlMode(CANTalon.ControlMode.Follower)
     leftPivotMotor.set(8)
     leftPivotMotor.reverseSensor(True)
     
-    def pivot(bool direction) {
+    def pivot(bool direction):
+        print(1)
             
             
 	
