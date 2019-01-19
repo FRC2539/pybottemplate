889b0483d4ba37320036c6cf1c49a9015c8dc1c9
Krypton Cougars <macbook1@kryptoncougars.pasd.us>
2019-01-18 21:06:20 -0500

    testing network tables

--- commands/autonomouscommandgroup.py
+++ commands/autonomouscommandgroup.py
@@ -2,8 +2,12 @@ from wpilib.command import CommandGroup
 import commandbased.flowcontrol as fc
 from custom.config import Config
 
+from commands.network.alertcommand import AlertCommand
+
 
 class AutonomousCommandGroup(CommandGroup):
 
     def __init__(self):
         super().__init__('Autonomous')
+
+        AlertCommand(Config('cameraInfo/cargoX'), 'Info')
