f84a205b279ec8de3440f20b61a3d0d5c7b19b76
Krypton Cougars <macbook1@kryptoncougars.pasd.us>
2020-01-25 14:24:12 -0500

    Added helpful sh's to master.

---
+++ download.sh
@@ -0,0 +1,4 @@
+robotpy-installer download-robotpy
+robotpy-installer download-opkg robotpy-rev
+robotpy-installer download-opkg robotpy-ctre
+robotpy-installer download robotpy-navx
---
+++ install.sh
@@ -0,0 +1,4 @@
+robotpy-installer install-robotpy
+robotpy-installer install-opkg robotpy-rev
+robotpy-installer install-opkg robotpy-ctre
+robotpy-installer install robotpy-navx
