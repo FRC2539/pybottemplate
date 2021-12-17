6fcfbb31679f6d97ed6d16ecfdada031dfa5a9f3
Ben <bbistline02@gmail.com>
2021-12-16 19:32:33 -0500

    Updated the generate.py to contain the correct paths in 2021.

--- generate.py
+++ generate.py
@@ -31,16 +31,25 @@ def generateSubsystem():
     with open("subsystems/%s.py" % module, "w") as f:
         f.write(
             """
-from wpilib.command import Subsystem
+from .cougarsystem import CougarSystem
 
 import ports
+import math
 
 
-class {subsystem}(Subsystem):
+class {subsystem}(CougarSystem):
     \'\'\'Describe what this subsystem does.\'\'\'
 
     def __init__(self):
         super().__init__('{subsystem}')
+        
+    def periodic(self):
+        \"\"\"
+        Loops when nothing else is running in
+        this subsystem. Do not call this!
+        \"\"\"
+        self.feed()
+        
 """.lstrip().format(
                 subsystem=subsystem
             )
@@ -77,11 +86,11 @@ class {subsystem}(Subsystem):
     with open("commands/resetcommand.py") as f:
         resetcommand = f.read()
 
-    requires = re.compile("(self\.requires\(robot.\w+\)\s*)+")
+    requires = re.compile("(self\.addRequirements\(robot.\w+\)\s*)+")
     match = requires.search(resetcommand)
 
     old = match[0].strip()
-    new = "%s\n        self.requires(robot.%s)"
+    new = "%s\n        self.addRequirements(robot.%s)"
     resetcommand = resetcommand.replace(old, new % (old, module))
 
     with open("commands/resetcommand.py", "w") as f:
@@ -111,17 +120,16 @@ def generateCommand():
 
     inherits = None
     if command.endswith("CommandGroup"):
-        inherits = "CommandGroup"
+        inherits = "CommandGroupBase"
     elif not command.endswith("Command"):
         command += "Command"
 
     if command == "DefaultCommand":
-        inherits = "Command"
+        inherits = "CommandBase"
 
     if inherits is None:
         bases = [
             "InstantCommand",
-            "TimedCommand",
             "CommandGroup",
             "Command",
             "DefaultCommand",
@@ -137,13 +145,16 @@ def generateCommand():
         if not inherits in bases:
             error("Unknown base class %s" % inherits)
 
+        if inherits == "Command" or inherits == "CommandGroup":
+            inherits += "Base"
+
     if inherits == "CommandGroup":
         inherits = "fc.CommandFlow"
         if command.endswith("Command"):
-            command += "Group"
+            command += "GroupBase"
 
     if inherits == "DefaultCommand":
-        inherits = "Command"
+        inherits = "CommandBase"
         command = "DefaultCommand"
 
     # Put spaces before capital letters
@@ -206,7 +217,7 @@ def generateCommand():
     if inherits == "fc.CommandFlow":
         content = "import commandbased.flowcontrol as fc"
     else:
-        content = "from wpilib.command import %s" % inherits
+        content = "from commands2 import %s" % inherits
 
         if len(requirements) > 0:
             content += "\n\nimport robot"
@@ -220,12 +231,12 @@ def generateCommand():
 
     if inherits == "TimedCommand":
         if duration:
-            content += "        super().__init__('%s', %s)"
+            content += "        super().__init__()"
             content = content % (description, duration)
         else:
-            content += "        super().__init__('%s', timeout)" % description
+            content += "        super().__init__()"
     else:
-        content += "        super().__init__('%s')" % description
+        content += "        super().__init__()"
 
     content += "\n\n"
 
@@ -236,7 +247,7 @@ def generateCommand():
     else:
         if len(requirements) > 0:
             for subsystem in requirements:
-                content += "        self.requires(robot.%s)\n" % subsystem
+                content += "        self.addRequirements(robot.%s)\n" % subsystem
 
             content += "\n"
 
@@ -244,7 +255,7 @@ def generateCommand():
 
         if inherits != "InstantCommand":
             content += "    def execute(self):\n        pass\n\n\n"
-            content += "    def end(self):\n        pass\n"
+            content += "    def end(self, interrupted):\n        pass\n"
 
     with open("%s/%s.py" % (path, command.lower()), "w") as f:
         f.write(content)
